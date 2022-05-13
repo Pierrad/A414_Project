de47805462be97b4dd93746b115b385a4c0ced4e
wassim <wassimbouzakri@icloud.com>
2022-05-13 12:13:26 +0200

    :lipstick: #french1 Add editText

--- app/src/main/java/com/example/project/french/dictation/DictationActivity.java
+++ app/src/main/java/com/example/project/french/dictation/DictationActivity.java
@@ -1,8 +1,16 @@
 package com.example.project.french.dictation;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,14 +18,136 @@ import com.example.project.R;
 import com.example.project.english.EnglishDashboardActivity;
 
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class DictationActivity extends AppCompatActivity {
+
+    TextView playerPosition, playerDuration;
+    SeekBar seekBar;
+    ImageView btRew, btPlay, btPause, btFf;
+    MediaPlayer mediaPlayer;
+    Handler handler = new Handler();
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dictation);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Dictation");
+
+        playerPosition = findViewById(R.id.player_position);
+        playerDuration = findViewById(R.id.player_duration);
+        seekBar = findViewById(R.id.seek_bar);
+        btRew = findViewById(R.id.bt_rew);
+        btPlay = findViewById(R.id.bt_play);
+        btPause = findViewById(R.id.bt_pause);
+        btFf = findViewById(R.id.bt_ff);
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.dictee1);
+
+
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+
+                handler.postDelayed(this,500);
+            }
+        };
+
+
+        int duration = mediaPlayer.getDuration();
+
+        //convert millisecond to minute and second
+        String sDuration = convertFormat(duration);
+
+        playerDuration.setText(sDuration);
+
+        btPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btPlay.setVisibility(View.GONE);
+                btPause.setVisibility(View.VISIBLE);
+                mediaPlayer.start();
+                seekBar.setMax(mediaPlayer.getDuration());
+                handler.postDelayed(runnable,0);
+            }
+        });
+
+        btPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btPlay.setVisibility(View.VISIBLE);
+                btPause.setVisibility(View.GONE);
+                mediaPlayer.pause();
+                handler.removeCallbacks(runnable);
+            }
+        });
+
+        btFf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentPosition = mediaPlayer.getCurrentPosition();
+                int duration = mediaPlayer.getDuration();
+
+                if (mediaPlayer.isPlaying() && duration != currentPosition){
+                    currentPosition += 5000;
+                    playerPosition.setText(convertFormat(currentPosition));
+                    mediaPlayer.seekTo(currentPosition);
+                }
+            }
+        });
+
+        btRew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentPosition = mediaPlayer.getCurrentPosition();
+
+                if(mediaPlayer.isPlaying() && currentPosition > 5000){
+                    currentPosition -= 5000;
+                    playerPosition.setText(convertFormat(currentPosition));
+                    mediaPlayer.seekTo(currentPosition);
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+                playerPosition.setText(convertFormat(mediaPlayer.getCurrentPosition()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                btPause.setVisibility(View.GONE);
+                btPlay.setVisibility(View.VISIBLE);
+                mediaPlayer.seekTo(0);
+            }
+        });
+    }
+
+    @SuppressLint("DefaultLocale")
+    private String convertFormat(int duration) {
+        return String.format("%02d:%02d"
+                ,TimeUnit.MILLISECONDS.toMinutes(duration)
+                ,TimeUnit.MILLISECONDS.toSeconds(duration) -
+                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)));
     }
 
 }
---
+++ app/src/main/java/com/example/project/helpers/MediaPlayer.java
@@ -0,0 +1,8 @@
+package com.example.project.helpers;
+
+public class MediaPlayer {
+
+    public MediaPlayer(){
+
+    }
+}
